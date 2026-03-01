4be20f111d028be8dd356b950dd0f22fd11aa59c
WilliamrDiaz <wrdiaz@unicauca.edu.co>
2026-02-28 22:10:51 -0500

    initial commit

--- app/src/main/java/com/byme/app/MainActivity.kt
+++ app/src/main/java/com/byme/app/MainActivity.kt
@@ -1,13 +1,9 @@
 package com.byme.app
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
